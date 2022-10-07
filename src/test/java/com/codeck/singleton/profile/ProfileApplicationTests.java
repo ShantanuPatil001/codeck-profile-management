27c19ab7c4bb036e5c8bb2960a1aec3c51a2fa0a
Shantanu Patil <56440842+shantanu706@users.noreply.github.com>
2022-10-07 22:08:34 +0530

    Initial Config

---
+++ src/test/java/com/codeck/singleton/profile/ProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeck.singleton.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
